656a0346820153a6174b2949a5258438cd05a799
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-10-10 16:12:51 +0500

    пз4

---
+++ ПЗ.4/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    private static object lockObject = new object();
+    static void Main(string[] args)
+    {
+        // Создаем несколько потоков, которые будут выполнять код одновременно
+        for (int i = 1; i <= 5; i++)
+        {
+            Thread thread = new Thread(SaveData);
+            thread.Name = "Thread " + i;
+            thread.Start();
+        }
+
+        Console.ReadLine();
+    }
+    private static void SaveData()
+    {
+        Console.WriteLine(Thread.CurrentThread.Name + " начал выполнение.");
+
+
+        lock (lockObject) // Блокируем код для выполнения в единственном экземпляре
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " вошел в критическую секцию.");
+
+            // Критическая секция кода
+            Console.WriteLine(Thread.CurrentThread.Name + " выполняет работу...");
+            Thread.Sleep(1000); // Имитируем выполнение работы
+
+            Console.WriteLine(Thread.CurrentThread.Name + " вышел из критической секции.");
+        }
+
+        Console.WriteLine(Thread.CurrentThread.Name + " завершил выполнение.");
+        Console.ReadLine();
+    }
+}
+
+
+
+
+

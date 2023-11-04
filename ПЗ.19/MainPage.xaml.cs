cd458ba6f5f1d20219db89021538c66d29d79516
hristinamineeva27@gmail.com <hristinamineeva27@gmail.com>
2023-11-04 14:34:24 +0500

    пз19

---
+++ ПЗ.19/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace ПЗ._19
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MySplitView.PaneClosed += MySplitView_PaneClosed;
+            MySplitView.PaneOpened += MySplitView_PaneOpened;
+        }
+
+        private void MySplitView_PaneClosed(SplitView sender, object args)
+        {
+            ActivationTextBlock.Text = "";
+        }
+
+        private void MySplitView_PaneOpened(SplitView sender, object args)
+        {
+            ActivationTextBlock.Text = "Успешная активация";
+        }
+
+        private void ShowPaneButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+    }
+}
